67b176f2e832af14ddd317f3e7ffa6fa674cc41f
Calvin Josenhans <60663079+HalflingHelper@users.noreply.github.com>
2023-12-01 09:32:28 -0500

    Day1!

---
+++ day1.lua
@@ -0,0 +1,51 @@
+DAY = 1
+
+local filename = string.format("inputs/input_%d.txt", DAY)
+
+local f = assert(io.open(filename, "r"))
+
+-- Function definitions here
+function subNums(t)
+    local nums = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" }
+
+    local newStr = ""
+
+    for i = 1, #t do
+        if t:match("()%d", i) == i then
+            newStr = newStr .. t:sub(i, i)
+        else
+            for j, v in ipairs(nums) do
+                if t:match("()" .. v, i) == i then
+                    newStr = newStr .. j
+                    break
+                end
+            end
+        end
+    end
+
+    return newStr
+end
+
+local part1_sum = 0
+local part2_sum = 0
+
+for line in f:lines() do
+    -- Process the file here
+    local nums = string.gsub(line, "%a", "")
+
+    local n1 = nums:sub(1, 1)
+    local n2 = nums:sub(#nums, #nums)
+
+    part1_sum = part1_sum + tonumber(n1) * 10 + tonumber(n2)
+
+    local nums = subNums(line)
+
+    local n1 = nums:sub(1, 1)
+    local n2 = nums:sub(#nums, #nums)
+
+    part2_sum = part2_sum + tonumber(n1) * 10 + tonumber(n2)
+end
+
+-- Do everything else here
+print("Part 1: ", part1_sum)
+print("Part 2: ", part2_sum)
---
+++ template.lua
@@ -0,0 +1,14 @@
+DAY = _
+
+local filename = string.format("inputs/input_%d.txt", DAY)
+
+local f = assert(io.open(filename, "r"))
+
+-- Function definitions here
+
+for line in f:lines() do
+    -- Process the file here
+
+end
+
+-- Do everything else here
